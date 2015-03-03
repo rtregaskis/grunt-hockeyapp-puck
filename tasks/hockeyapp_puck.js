0c79c59ffef42ac815c58f27e8820932b8ebe2fd
rtregaskis <rtregaskis@dresources.com>
2015-03-03 14:33:56 +0000

    warn on missing/invalid options. use `request.post` instead of `get`. clean up logging messages

--- tasks/hockeyapp_puck.js
+++ tasks/hockeyapp_puck.js
@@ -8,7 +8,8 @@
 
 
 var request = require('request'),
-	fs = require('fs');
+    fs = require('fs'),
+	path = require('path');
 
 module.exports = function(grunt) {
     'use strict';
@@ -16,7 +17,7 @@ module.exports = function(grunt) {
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
-    grunt.registerMultiTask('hockeyapp_puck', 'Interface to HockeyApp for grunt', function() {
+    grunt.registerMultiTask('hockeyapp_puck', 'Upload builds to HockeyApp via grunt', function() {
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
             token: null,
@@ -26,44 +27,61 @@ module.exports = function(grunt) {
             notify: 2,
             status: 2,
             tags: '',
-			platform: null
         });
 
-		console.log(options);
+		// warn and exit on missing options
+        if (!options['token'] || options['token'] === undefined || options['token'] === '') {
+            return grunt.warn(
+                'Token option is required!'
+            );
+        } else if (!options['app_id'] || options['app_id'] === undefined || options['app_id'] === '') {
+            return grunt.warn(
+                'Application id option is required!'
+            );
+        } else if (!options['file'] || options['file'] === undefined || options['file'] === '') {
+            return grunt.warn(
+                'File option is required!'
+            );
+        } else if (!path.existsSync(options['file'])) {
+            return grunt.warn(
+                'File option target does not exist!'
+            );
+		}
 
-		var formData = {
-			ipa : fs.createReadStream(options['file']),
-			status:options.status,
-			notify:options.notify,
-			notes:options.notes,
-			notes_type:1
-		};
+        // construct form data
+        // NB: use read stream to access IPA file
+        var formData = {
+            ipa: fs.createReadStream(options['file']),
+            status: options.status,
+            notify: options.notify,
+            notes: options.notes,
+            notes_type: 1
+        };
 
-	
-		var url = 'https://rink.hockeyapp.net/api/2/apps/' + options.app_id + '/app_version/upload';
+        // tidy up url
+        var url = 'https://rink.hockeyapp.net/api/2/apps/' + options.app_id + '/app_versions/upload';
 
-
-        // warn on missing options
-
+        // run this asynchronously
         var done = this.async();
 
-        request.get({
+        grunt.log.subhead('Uploading "' + options['file'] + '"');
+
+        // fire request to server.
+        request.post({
             url: url,
-			formData:formData,
+            formData: formData,
             headers: {
                 'X-HockeyAppToken': options['token']
             }
         }, function(error, response, body) {
-	            console.log(body);
-			if(error !== null){
-				grunt.log.error(error);
-				grunt.log.rror('Error uploading "'+options['file']+'"');
-				done(false);
-			}else{
-				grunt.log.ok('Uploaded "'+options['file']+'"');
-				done();
-			}
+            if (error !== null) {
+                grunt.log.error(error);
+                grunt.log.error('Error uploading "' + options['file'] + '"');
+                done(false);
+            } else {
+                grunt.log.ok('Uploaded "' + options['file'] + '" successfully');
+                done();
+            }
         });
     });
-
 };
